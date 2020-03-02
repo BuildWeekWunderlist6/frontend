c3be62407e6f739922445821f0d1aca0f5714f15
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-02 14:01:35 -0500

    Complete Login component, change 'Sign Up' to 'Register' and 'Sign In' to 'Login'

---
+++ src/components/Login.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
+
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+
+
+const Login = ({values, handleChange, touched, errors, status},{...props}) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        console.log("Status has changed", status);
+        status && setUsers(users => [...users, status]);
+    }, [status]);
+    
+    return (
+        <div className = "form">
+            <h2>Login</h2>
+           
+        <Form>
+            <label htmlFor = "email"> Email
+            <Field id = "email" type = "email" name = "email" placeholder = "Email" />
+            {touched.email && errors.email && (
+                <p className = "error"> {errors.email}</p>
+            )}
+            </label>
+            <br/>
+            <label htmlFor = "password"> Password
+            <Field id = "password" type = "password" name = "password" placeholder = "Password" />
+            {touched.password && errors.password && (
+                <p className = "error">{errors.password}</p>
+            )}
+            </label>
+            <br/>
+            <button type = "submit">Submit</button>
+            
+        </Form>
+        </div>
+    );
+};
+
+const FormikLogin = withFormik({
+    mapPropsToValues({email, password}) {
+        return {
+            email: email || "",
+            password: password || ""
+        };
+    },
+    validationSchema: Yup.object().shape({
+        email: Yup.string().required("Email is required."),
+        password: Yup.string().required("Password is required.")
+    }),
+    
+    handleSubmit(values, { setStatus, resetForm }) {
+        console.log("Submitting", values);
+        axiosWithAuth().post("/users/login", values)
+            .then(response => {
+                window.localStorage.setItem('token', response.data.payload);
+                console.log("VALUES", values);
+                console.log("Success", response.data);
+                setStatus(response.data);
+               
+                resetForm();
+            })
+            .catch(response => {
+                console.log("Error posting data", response);
+            });
+    }
+})(Login);
+
+export default FormikLogin;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,28 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Link, Route, Switch } from "react-router-dom";
-import SignIn from "./SignIn";
+import Register from "./Register";
+import Login from "./Login";
 
 export default function Navigation() {
     return (
         <Router>
         <div class = "navigation">
-        <h1>Wunderlist 2.0</h1>
         <nav>
             <Link to = "/">Home</Link>
             <Link to = "/login">Login</Link>
             <Link to = "/register">Register</Link>
         </nav>
+        </div>
         <Switch>
         <Route path = "/login">
-          <SignIn/>
+            <Login/>
         </Route>
   
         <Route path = "/register">
-          <SignIn/>
+          <Register/>
         </Route>
         </Switch>
-        </div>
         </Router>
     );
 }
