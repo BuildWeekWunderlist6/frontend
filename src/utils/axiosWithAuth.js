24e704ed66c130f22444553d11239f9bb63f6a88
kaverndsp <kaverndsp@users.noreply.github.com>
2020-03-02 12:10:37 -0600

    axiosWithAuth & PrivateRoute

---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+export const axiosWithAuth = () => {
+  // get the token from localstorage
+  const token = window.localStorage.getItem('token');
+  // create a new "instance" of axios with the config object built into it
+  return axios.create({
+    headers: {
+      authorization: token
+    },
+    baseURL: 'https://ls-wunderlist--production.herokuapp.com/api'
+  });
+};
