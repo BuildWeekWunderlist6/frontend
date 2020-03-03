ac684101f16272ae0e2eb6b212b07f7e6ce43fc9
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-03 18:27:38 -0500

    Home, login, and register components designed as cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+
 
 export default function Home() {
     return (
-        <div class = "homepage">
+        <div className = "maincard">
+        <div className = "homepage">
+            <div className = "hometext">
             <h2>Welcome to the home page!</h2>
+            <p>Need to access the dashboard? <Link to = "/login">Login</Link></p>
+            </div>
+        </div>
         </div>
     )};
