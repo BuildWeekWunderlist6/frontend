fb543507ce1c4d25face7bcad5e5a94c24527fac
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-03 18:29:02 -0500

    Update site title and tweak wording

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div className = "maincard">
         <div className = "homepage">
             <div className = "hometext">
-            <h2>Welcome to the home page!</h2>
+            <h2>Welcome to the homepage!</h2>
             <p>Need to access the dashboard? <Link to = "/login">Login</Link></p>
             </div>
         </div>
